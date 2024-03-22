e6aa12bd8fd7061b9a66a6d3704b9a9ae5150050
Tavis <github@tavis.la>
2024-03-21 22:44:29 -1000

    display came alive !

--- variants/my_esp32s3_diy_oled/variant.h
+++ variants/my_esp32s3_diy_oled/variant.h
@@ -2,13 +2,13 @@
 #undef GPS_RX_PIN
 #undef GPS_TX_PIN
 
-// #define HAS_SCREEN 0
+#define HAS_SCREEN 0
 // #define HAS_SDCARD
 // #define SDCARD_USE_SPI1
 
 #define HAS_RADIO 0
 
-#define USE_SSD1306
+// #define USE_SSD1306
 
 #define I2C_SDA 18 // 1 // I2C pins for this board
 #define I2C_SCL 17 // 2
@@ -22,27 +22,27 @@
 // #define USE_SX1262
 #define USE_SX1280
 
-#define LORA_MISO 3
-#define LORA_SCK 5
-#define LORA_MOSI 6
-#define LORA_CS 7
+#define LORA_MISO -1
+#define LORA_SCK -1
+#define LORA_MOSI -1
+#define LORA_CS -1
 
-#define LORA_RESET 8
-#define LORA_DIO1 16
+#define LORA_RESET -1
+#define LORA_DIO1 -1
 
 #ifdef USE_SX1262
 #define SX126X_CS LORA_CS // FIXME - we really should define LORA_CS instead
 #define SX126X_DIO1 LORA_DIO1
-#define SX126X_BUSY 15
+#define SX126X_BUSY -1
 #define SX126X_RESET LORA_RESET
-#define SX126X_RXEN 4
-#define SX126X_TXEN 9
+#define SX126X_RXEN -1
+#define SX126X_TXEN -1
 #endif
 
 #ifdef USE_SX1280
 #define SX128X_CS LORA_CS
 #define SX128X_DIO1 LORA_DIO1
-#define SX128X_BUSY 15
+#define SX128X_BUSY -1
 #define SX128X_RESET LORA_RESET
 #endif
 
